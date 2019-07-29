e29575819147923ac680efe892f3ddcdbc462177
zouxin <zouxin@iboxpay.com>
2019-07-29 17:39:51 +0800

    fix: 修正 publicPath，同步为新项目名称

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/vue-travis-ci/'
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/engineering-project/'
 }
